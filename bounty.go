aac66ab4fc25033d34fdb8a4fc35fd9ed2cef3e0
dgduncan <derekgduncan@gmail.com>
2020-08-22 17:46:44 -0500

    added more info to Bounty struct including new structs

--- bounty.go
+++ bounty.go
@@ -21,15 +21,24 @@ type BountyService struct {
 	Client *Client
 }
 
+type BountyResponse struct {
+	Bounties []Bounty `json:"bounties,omitempty"`
+}
+
 type Bounty struct {
-	UUID       string
-	BountyType string
-	Code       string
-	Status     string
+	UUID              string             `json:"uuid,omitempty"`
+	BountyType        string             `json:"bountytype,omitempty"`
+	Code              string             `json:"code,omitempty"`
+	CustomFieldLabels []CustomFieldLabel `json:"custom_field_labels,omitempty"`
+	Status            string             `json:"status,omitempty"`
+}
+type CustomFieldLabel struct {
+	FieldID   string `json:"field_id,omitempty"`
+	FieldName string `json:"field_name,omitempty"`
 }
 
 // GetBounties retrieves all bounty information from Bugcrowd that the you have access
-func (b *BountyService) GetBounties() ([]Bounty, error) {
+func (b *BountyService) GetBounties() (BountyResponse, error) {
 	u := b.Client.BaseURL
 	u.Path = path.Join(u.Path, getBountiesEndpoint)
 	req, _ := http.NewRequest(http.MethodGet, u.String(), http.NoBody)
@@ -38,20 +47,20 @@ func (b *BountyService) GetBounties() ([]Bounty, error) {
 
 	resp, err := b.Client.http.Do(req)
 	if err != nil {
-		return nil, err
+		return BountyResponse{}, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		return []Bounty{}, fmt.Errorf("BugCrowd returned non 200: %d", resp.StatusCode)
+		return BountyResponse{}, fmt.Errorf("BugCrowd returned non 200: %d", resp.StatusCode)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return BountyResponse{}, err
 	}
 
-	var bounties []Bounty
+	var bounties BountyResponse
 	json.Unmarshal(body, &bounties)
 
 	return bounties, nil
