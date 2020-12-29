1e672bc13db066c41adf8aaf95e863490e6757a1
Derek Duncan <derekgduncan@gmail.com>
2020-12-28 23:16:09 -0500

    added new Do for bugcrowd client

--- bounty.go
+++ bounty.go
@@ -1,9 +1,7 @@
 package bugcrowd
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"context"
 	"net/http"
 	"net/url"
 	"path"
@@ -16,8 +14,8 @@ const (
 
 // BountyAPI is the interface used for mocking of Bounty API calls
 type BountyAPI interface {
-	GetBounties(requestConfig GetBountiesRequestConfig) (GetBountiesResponse, error)
-	RetrieveBounty(uuid string) (RetrieveBountyResponse, error)
+	GetBounties(ctx context.Context, requestConfig GetBountiesRequestConfig) (*http.Response, *GetBountiesResponse, error)
+	RetrieveBounty(ctx context.Context, uuid string) (*http.Response, *RetrieveBountyResponse, error)
 }
 
 // BountyService test
@@ -38,36 +36,38 @@ type GetBountiesRequestQueryParams struct {
 
 // GetBountiesResponse is the wrapper object returned by Bugcrowd in its GetBounty response
 type GetBountiesResponse struct {
-	Bounties []Bounty `json:"bounties,omitempty"`
+	Bounties []*Bounty `json:"bounties,omitempty"`
 }
 
 // RetrieveBountyResponse test
 type RetrieveBountyResponse struct {
-	Bounty Bounty `json:"bounty,omitempty"`
+	Bounty *Bounty `json:"bounty,omitempty"`
 }
+
+// TODO : add stringify
 
 // Bounty test
 type Bounty struct {
-	UUID                    string             `json:"uuid,omitempty"`
-	BountyType              string             `json:"bountytype,omitempty"`
-	Code                    string             `json:"code,omitempty"`
-	CustomFieldLabels       []CustomFieldLabel `json:"custom_field_labels,omitempty"`
-	DescriptionMarkdown     string             `json:"description_markdown,omitempty"`
-	Demo                    bool               `json:"demo,omitempty"`
-	EndsAt                  time.Time          `json:"ends_at,omitempty"`
-	HighRewards             int                `json:"high_rewards,omitempty"`
-	LowRewards              int                `json:"low_rewards,omitempty"`
-	Participation           string             `json:"participation,omitempty"`
-	PointsOnly              bool               `json:"points_only,omitempty"`
-	StartsAt                time.Time          `json:"starts_at,omitempty"`
-	TargetsOverviewMarkdown string             `json:"targets_overview_markdown,omitempty"`
-	Tagline                 string             `json:"tagline,omitempty"`
-	TotalPrizePool          string             `json:"total_prize_pool,omitempty"`
-	RemainingPrizePool      string             `json:"remaining_prize_pool,omitempty"`
-	Trial                   bool               `json:"trial,omitempty"`
-	Status                  string             `json:"status,omitempty"`
-	ServiceLevel            string             `json:"service_level,omitempty"`
-	Organization            Organization       `json:"organization,omitempty"`
+	UUID                    *string             `json:"uuid,omitempty"`
+	BountyType              *string             `json:"bountytype,omitempty"`
+	Code                    *string             `json:"code,omitempty"`
+	CustomFieldLabels       *[]CustomFieldLabel `json:"custom_field_labels,omitempty"`
+	DescriptionMarkdown     *string             `json:"description_markdown,omitempty"`
+	Demo                    *bool               `json:"demo,omitempty"`
+	EndsAt                  *time.Time          `json:"ends_at,omitempty"`
+	HighRewards             *int                `json:"high_rewards,omitempty"`
+	LowRewards              *int                `json:"low_rewards,omitempty"`
+	Participation           *string             `json:"participation,omitempty"`
+	PointsOnly              *bool               `json:"points_only,omitempty"`
+	StartsAt                *time.Time          `json:"starts_at,omitempty"`
+	TargetsOverviewMarkdown *string             `json:"targets_overview_markdown,omitempty"`
+	Tagline                 *string             `json:"tagline,omitempty"`
+	TotalPrizePool          *string             `json:"total_prize_pool,omitempty"`
+	RemainingPrizePool      *string             `json:"remaining_prize_pool,omitempty"`
+	Trial                   *bool               `json:"trial,omitempty"`
+	Status                  *string             `json:"status,omitempty"`
+	ServiceLevel            *string             `json:"service_level,omitempty"`
+	Organization            *Organization       `json:"organization,omitempty"`
 }
 
 // Organization represents the organization a given bounty belongs.
@@ -77,67 +77,43 @@ type Organization struct {
 }
 
 // GetBounties retrieves all bounty information from Bugcrowd that the you have access
-func (b *BountyService) GetBounties(requestConfig GetBountiesRequestConfig) (GetBountiesResponse, error) {
+func (b *BountyService) GetBounties(ctx context.Context, requestConfig GetBountiesRequestConfig) (*http.Response, *GetBountiesResponse, error) {
 	u, _ := url.Parse(b.client.BaseURL.String())
 	u.Path = path.Join(u.Path, commonBountiesEndpoint)
 
 	req, err := http.NewRequest(http.MethodGet, u.String(), http.NoBody)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/vnd.bugcrowd+json")
+	req.Header.Set("Accept", bugcrowdJSONAccept)
 
 	q := req.URL.Query()
 	q.Add("limit", requestConfig.QueryParams.Limit)
 	q.Add("offset", requestConfig.QueryParams.Offset)
 	req.URL.RawQuery = q.Encode()
 
-	resp, err := b.client.http.Do(req)
+	bounties := new(GetBountiesResponse)
+	resp, err := b.client.Do(ctx, req, bounties)
 	if err != nil {
-		return GetBountiesResponse{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return GetBountiesResponse{}, fmt.Errorf("BugCrowd returned non 200: %d", resp.StatusCode)
+		return resp, &GetBountiesResponse{}, err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return GetBountiesResponse{}, err
-	}
-
-	var bounties GetBountiesResponse
-	json.Unmarshal(body, &bounties)
-
-	return bounties, nil
+	return resp, bounties, nil
 }
 
 // RetrieveBounty retrieves bounty with the given UUID
 // If bounty with given ID is not found, an empty response will be returned with a nil error
-func (b *BountyService) RetrieveBounty(uuid string) (RetrieveBountyResponse, error) {
+func (b *BountyService) RetrieveBounty(ctx context.Context, uuid string) (*http.Response, *RetrieveBountyResponse, error) {
 	u, _ := url.Parse(b.client.BaseURL.String())
 	u.Path = path.Join(u.Path, commonBountiesEndpoint, uuid)
 
 	req, _ := http.NewRequest(http.MethodGet, u.String(), http.NoBody)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "application/vnd.bugcrowd+json")
+	req.Header.Set("Accept", bugcrowdJSONAccept)
 
-	resp, err := b.client.http.Do(req)
+	bounty := new(RetrieveBountyResponse)
+	resp, err := b.client.Do(ctx, req, bounty)
 	if err != nil {
-		return RetrieveBountyResponse{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return RetrieveBountyResponse{}, fmt.Errorf("BugCrowd returned non 200: %d", resp.StatusCode)
+		return resp, &RetrieveBountyResponse{}, err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return RetrieveBountyResponse{}, err
-	}
-
-	var bounty RetrieveBountyResponse
-	json.Unmarshal(body, &bounty)
-
-	return bounty, nil
+	return resp, bounty, nil
 }
--- bugcrowd.go
+++ bugcrowd.go
@@ -1,12 +1,18 @@
 package bugcrowd
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 	"net/url"
 )
 
 const (
 	baseURL = "https://api.bugcrowd.com"
+
+	bugcrowdJSONAccept = "application/vnd.bugcrowd+json"
 )
 
 // Client represents the basic struct for the Bugcrowd client
@@ -37,4 +43,41 @@ func NewClient(auth BasicAuth) (*Client, error) {
 	c.Bounty = &BountyService{client: c}
 
 	return c, nil
+}
+
+// Do test
+func (c *Client) Do(ctx context.Context, r *http.Request, b interface{}) (*http.Response, error) {
+	if ctx == nil {
+		return nil, errors.New("must pass a non-nil context")
+	}
+	r = r.WithContext(ctx)
+
+	resp, err := c.http.Do(r)
+	if err != nil {
+		// TODO : check if context error ocurred
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return resp, err
+	}
+
+	// check if b is null as interface as nullable when passed
+	if b != nil {
+		if w, ok := b.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			decErr := json.NewDecoder(resp.Body).Decode(b)
+			if decErr == io.EOF {
+				decErr = nil // ignore EOF errors caused by empty response body
+			}
+			if decErr != nil {
+				err = decErr
+			}
+		}
+	}
+
+	return resp, err
 }
